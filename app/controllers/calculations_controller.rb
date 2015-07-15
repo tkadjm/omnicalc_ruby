abf6f5175370bd5906de0bee673443deaf89a741
tkadjm <tkeane@chicagobooth.edu>
2015-07-15 16:25:33 -0500

    completes part 3

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -4,20 +4,13 @@ class CalculationsController < ApplicationController
     @text = params[:user_text]
     @special_word = params[:user_word]
 
-    # ================================================================================
-    # Your code goes below.
-    # The text the user input is in the string @text.
-    # The special word the user input is in the string @special_word.
-    # ================================================================================
+    @character_count_with_spaces = @text.length
 
+    @character_count_without_spaces = @text.gsub(" ", "").length
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @word_count = @text.split.length
 
-    @character_count_without_spaces = "Replace this string with your answer."
-
-    @word_count = "Replace this string with your answer."
-
-    @occurrences = "Replace this string with your answer."
+    @occurrences = @text.downcase.split.count(@special_word.downcase)
   end
 
   def loan_payment
@@ -25,35 +18,27 @@ class CalculationsController < ApplicationController
     @years = params[:number_of_years].to_i
     @principal = params[:principal_value].to_f
 
-    # ================================================================================
-    # Your code goes below.
-    # The annual percentage rate the user input is in the decimal @apr.
-    # The number of years the user input is in the integer @years.
-    # The principal value the user input is in the decimal @principal.
-    # ================================================================================
+    def payment (rate, term, bal)
+        r = rate / 1200       # convert to monthly decimal rate
+        n = r * bal           # numerator of the payment formula
+        t = term * 12         # convert term to months
+        d = 1 - (1+r)**-t     # denominator of the payment formula
+        pmt = n/d             # return montly payment
+    end
 
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = payment(@apr,@years,@principal)
   end
 
   def time_between
     @starting = Chronic.parse(params[:starting_time])
     @ending = Chronic.parse(params[:ending_time])
 
-    # ================================================================================
-    # Your code goes below.
-    # The start time is in the Time @starting.
-    # The end time is in the Time @ending.
-    # Note: Ruby stores Times in terms of seconds since Jan 1, 1970.
-    #   So if you subtract one time from another, you will get an integer
-    #   number of seconds as a result.
-    # ================================================================================
-
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending - @starting
+    @minutes = @seconds / 60
+    @hours = @minutes / 60
+    @days = @hours / 24
+    @weeks = @days / 7
+    @years = @weeks / 52.1785714 # we could get fancy with leap years and whatnot later
   end
 
   def descriptive_statistics
@@ -64,26 +49,55 @@ class CalculationsController < ApplicationController
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @range = "Replace this string with your answer."
+    @range = @maximum - @minimum
 
-    @median = "Replace this string with your answer."
+    @median = (@sorted_numbers[(@count-1)/2]+@sorted_numbers[@count/2])/2.0
 
-    @sum = "Replace this string with your answer."
+    @sum = @numbers.sum
 
-    @mean = "Replace this string with your answer."
+    @mean = @sum / @count
 
-    @variance = "Replace this string with your answer."
+    def variance_calc (array_name, array_length, array_mean)
+        s = 0.0
+        for i in 0..array_length-1
+            s = s + (array_name[i] - array_mean)**2
+        end
+        array_variance = s / array_length
+    end
 
-    @standard_deviation = "Replace this string with your answer."
+    @variance = variance_calc(@numbers, @count, @mean)
 
-    @mode = "Replace this string with your answer."
+    @standard_deviation = Math.sqrt(@variance)
+
+    def mode_calc(array_element, array_length)          # this will work on an unsorted array
+
+        occurrences_on_this_iteration = 0               # counter
+        most_occurrences = 0                            # counter
+
+        for i in 0..array_length-1                      # loop to pick each sorted array element
+            test_element = array_element[i]             # pick the array element to test
+            for j in 0..array_length-1                  # loop to compare test_element to array_element
+                if test_element == array_element[j]
+                    occurrences_on_this_iteration = occurrences_on_this_iteration + 1
+                end
+            end
+            if occurrences_on_this_iteration > most_occurrences
+                mode_so_far = test_element
+                most_occurrences = occurrences_on_this_iteration
+            end
+            occurrences_on_this_iteration = 0
+        end
+        mode = mode_so_far
+    end
+
+    @mode = mode_calc(@sorted_numbers, @count)          # I still call it on a sorted array
   end
 end
